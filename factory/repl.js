a943fbf31fc2ef6dd0691df9bc9d563c0c5a146a
trapcodeio <trapcode.io@gmail.com>
2020-11-12 06:11:54 +0100

    Update Xpresser Repl

--- factory/repl.js
+++ factory/repl.js
@@ -7,14 +7,15 @@ repl.setConfigProvider(() => {
         env: process.env.NODE_ENV,
         paths: {base: __dirname}
     }
-})
+});
 
 /**
  * Start The Repl Server
  * By Booting xpresser using your config.
  *
- * $ (i.e xpresserInstance) is passed as the only argument.
  * repl.server is now defined.
+ *
+ * @param $ - Current xpresser instance.
  */
 repl.start(($) => {
     // Any Customization to the repl server `repl.server` directly can be done here.
