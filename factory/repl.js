3b471b836fdbac71fe7b72c169c29446431d7f48
trapcodeio <trapcode.io@gmail.com>
2020-11-12 09:54:48 +0100

    Update Xpresser Repl

--- factory/repl.js
+++ factory/repl.js
@@ -1,6 +1,7 @@
 const {XpresserRepl} = require('xpresser');
 const repl = new XpresserRepl();
 
+// Set Default config provider.
 repl.setConfigProvider(() => {
     // return your xpresser config
     return {
@@ -9,18 +10,25 @@ repl.setConfigProvider(() => {
     }
 });
 
-/**
- * Start The Repl Server
- * By Booting xpresser using your config.
- *
- * repl.server is now defined.
- *
- * @param $ - Current xpresser instance.
- */
-repl.start(($) => {
-    // Any Customization to the repl server `repl.server` directly can be done here.
 
+/**
+ * Run some tasks or add context before repl starts
+ * repl.server is undefined.
+ * @param $ - Current Xpresser Instance
+ */
+repl.beforeStart($ => {
     // Add Example Context to repl
     repl.addContext('example', () => 'Example content, modify in repl file.');
 });
+
+
+/**
+ * Start Repl Server.
+ * Function will run after repl server starts.
+ * @param $ - Current Xpresser Instance
+ */
+repl.start($ => {
+    // repl.server is now defined.
+    // Any Customization to the repl server `repl.server` directly can be done here.
+}).catch(console.error);
 
--- factory/repl_with_config.js
+++ factory/repl_with_config.js
@@ -1,19 +1,24 @@
-#!/usr/bin/env node --experimental-repl-await
 const {XpresserRepl} = require('xpresser');
 const repl = new XpresserRepl('{{configFile}}');
 
 /**
- * Start The Repl Server
- * By Booting xpresser using your config.
- *
- * repl.server is now defined.
- *
- * @param $ - Current xpresser instance.
+ * Run some tasks or add context before repl starts
+ * repl.server is undefined.
+ * @param $ - Current Xpresser Instance
  */
-repl.start(($) => {
-    // Any Customization to the repl server `repl.server` directly can be done here.
-
+repl.beforeStart($ => {
     // Add Example Context to repl
     repl.addContext('example', () => 'Example content, modify in repl file.');
 });
+
+
+/**
+ * Start Repl Server.
+ * Function will run after repl server starts.
+ * @param $ - Current Xpresser Instance
+ */
+repl.start($ => {
+    // repl.server is now defined.
+    // Any Customization to the repl server `repl.server` directly can be done here.
+}).catch(console.error);
 
